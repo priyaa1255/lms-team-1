d6143533ae41f3914459fad96a87aecc2c6a0749
priyaa1255 <125294288+priyaa1255@users.noreply.github.com>
2024-01-08 18:28:32 +0530

    Update firewall.tf

--- Backend-tf/firewall.tf
+++ Backend-tf/firewall.tf
@@ -1,1 +1,16 @@
-#ins
+provider "aws" {
+  region = "us-east-1" 
+}
+
+resource "aws_security_group" "aws_sg" {
+  name        = "sg-07d05ad1d488df536"
+  description = "Security Group"
+
+  ingress {
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"] 
+  }
+}
+
